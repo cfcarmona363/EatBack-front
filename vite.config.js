06eca447388028747467b08eeb22695cd42fc33b
Francisco Carmona <cfcarmona363@gmail.com>
2024-03-27 11:06:38 -0300

    Remove vite config

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,4 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), splitVendorChunkPlugin()],
-  build: {
-    rollupOptions: {
-      external: ["react", "react-router"],
-      output: {
-        globals: {
-          react: "React",
-        },
-      },
-    },
-  },
 });
